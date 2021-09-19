6538035a6c294c0c4fd113baac0f34fe65c73f8d
danielwilfredo <xvwilito11@gmail.com>
2021-09-18 18:46:47 -0600

    añadiendo plantilla base para los componentes

---
+++ src/components/estructuraComponente.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const nombreComponente = () => {
+  return (
+    <>
+      <Text>contenido del componente</Text>
+    </>
+  );
+};
+
+export default nombreComponente;
