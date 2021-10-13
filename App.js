5952114ba4a3ffca580f719eb7e3e8e653359add
Daniela Flores <79023027+DanyFP@users.noreply.github.com>
2021-10-13 09:37:49 -0600

    Screens

--- App.js
+++ App.js
@@ -1,21 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  Platform,
+  SafeAreaView,
+} from 'react-native';
+import Home from './Screens/Home';
+import Habitaciones from './Screens/Habitaciones';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons,} from '@expo/vector-icons';
+import Colors from './src/utils/colors';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRoutename="Habitaciones"
+      tabBarOptions={{
+        tabStyle: {
+          height: 100,
+        },
+        style: {
+          height: 100,
+        },
+      }}
+      screenOptions={{
+        tabBarActiveTintColor: Colors.FONDO,
+        tabBarActiveBackgroundColor: '#FFFFFF',
+        tabBarInactiveTintColor: '#A4A4A4',
+        tabBarInactiveBackgroundColor: 'FFFFFF',
+        showLabel: false,
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="home"
+              style={styles.iconos}
+              color={color}
+              size={30}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Habitaciones"
+        component={Habitaciones}
+        options={{
+          headerShown: false,
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="bed"
+              style={styles.iconos}
+              color={color}
+              size={30}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Reserva"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="calendar-edit"
+              style={styles.iconos}
+              color={color}
+              size={30}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-circle"
+              style={styles.iconos}
+              color={color}
+              size={30}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  iconos: {
+    marginBottom: 35,
   },
 });
---
+++ Screens/Habitaciones.js
@@ -0,0 +1,170 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Modal,
+  Button,
+  TouchableHighlight,
+  Platform,
+  FlatList,
+  Dimensions,
+  ImageBackground,
+} from 'react-native';
+import Colors from '../src/utils/colors';
+import Rooms from '../src/utils/rooms';
+
+
+export default function Habitaciones() {
+  const [habitacion, setHabitacion] = useState(false);
+  return (
+    <>
+      <View style={styles.v1}>
+        <ImageBackground
+          source={require('../src/img/SandCorner2.png')}
+          resizeMode="contain"
+          style={styles.image}></ImageBackground>
+        <View style={styles.vtitle}>
+          <Text style={styles.title}>Habitaciones</Text>
+        </View>
+      </View>
+      <ScrollView
+        style={styles.fondo}
+        vertical
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          overflow: 'hidden',
+          height:'100%',
+          backgroundColor:'#F7F7F7'
+        }}>
+        <View style={styles.raya}></View>
+        <View style={styles.viewwhite}>
+          <FlatList
+            vertical={true}
+            data={Rooms}
+            renderItem={({ item }) => (
+              <View>
+                <TouchableHighlight
+                  underlayColor="rgba(73,182,77,1,0.9)"
+                  onPress={() => {
+                    setHabitacion(!habitacion);
+                  }}>
+                  <View style={styles.objectview}>
+                    <Image style={styles.imghabit} source={item.img} />
+                    <View style={styles.viewtext}>
+                      <Text style={styles.nameroom}>{item.roomname}</Text>
+                      <Text style={styles.minis}>
+                        {item.numberbeds} | {item.numberbath} |{' '}
+                        {item.numberbalc}
+                      </Text>
+                      <Text style={styles.minis}>
+                        Para {item.numberhuespe} | {item.specialdetails}
+                      </Text>
+                      <Text style={styles.price}>{item.price}</Text>
+                    </View>
+                  </View>
+                </TouchableHighlight>
+              </View>
+            )}
+          />
+        </View>
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  fondo: {
+    backgroundColor: Colors.FONDO,
+    flex: 1,
+    marginTop: -525,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    overflow: 'hidden',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: 200,
+    marginTop: -70,
+  },
+  v1: {
+    flex: 1,
+    backgroundColor: Colors.FONDO,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  vtitle: {
+    flex: 1,
+    marginTop: Platform.OS == 'ios' ? 60 : 50,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    color: '#FFFFFF',
+  },
+  viewwhite: {
+    backgroundColor: '#F7F7F7',
+    flex: 1,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    paddingTop: Platform.OS == 'ios' ? 10 : 12,
+  },
+  imghabit: {
+    width: '92%',
+    height: 200,
+    borderRadius: 20,
+  },
+  objectview: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: Platform.OS == 'ios' ? 4 : 3,
+  },
+  viewtext: {
+    height: Platform.OS == 'ios' ? 90 : 102,
+    width: '80%',
+    backgroundColor: 'white',
+    marginRight: 5,
+    borderRadius: 20,
+    elevation: 6,
+    shadowColor: '#000',
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    marginBottom: 10,
+    marginTop: -30,
+    marginLeft: 5,
+    flex: 1,
+  },
+  nameroom: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingTop: 5,
+    textAlign: 'center',
+  },
+  minis: {
+    fontSize: 13,
+    textAlign: 'left',
+    paddingLeft: 25,
+    paddingTop: 1,
+    marginTop: Platform.OS == 'ios' ? 0 : -3,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: Colors.PRICE,
+    textAlign: 'right',
+    paddingRight: 25,
+    paddingTop: Platform.OS == 'ios' ? 2 : 0,
+    marginTop: Platform.OS == 'ios' ? 0 : -2,
+  },
+});
