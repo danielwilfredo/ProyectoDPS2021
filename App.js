1513e59056a9dbb256dfa04b8b87ca6cc6e429f0
Daniela Flores <79023027+DanyFP@users.noreply.github.com>
2021-10-15 21:15:33 -0600

    Cambios

--- App.js
+++ App.js
@@ -1,117 +1,15 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  ScrollView,
-  Platform,
-  SafeAreaView,
-} from 'react-native';
-import Home from './Screens/Home';
-import Habitaciones from './Screens/Habitaciones';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons,} from '@expo/vector-icons';
-import Colors from './src/utils/colors';
+import {NavigationContainer} from '@react-navigation/native';
+import { AuthProvider } from './src/Context/AuthContext';
+import AuthNavigation from './src/Navigator/AuthNavigation';
 
 export default function App() {
   return (
+    <AuthProvider>
     <NavigationContainer>
-      <MyTabs />
+      <AuthNavigation /> 
     </NavigationContainer>
+    </AuthProvider>
   );
 }
 
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      initialRoutename="Habitaciones"
-      screenOptions={{
-        tabBarActiveTintColor: Colors.FONDO,
-        tabBarActiveBackgroundColor: '#FFFFFF',
-        tabBarInactiveTintColor: '#A4A4A4',
-        tabBarInactiveBackgroundColor: 'FFFFFF',
-        showLabel: false,
-        tabStyle: {
-          height: 100,
-        },
-        style: {
-          height: 100,
-        },
-      }}>
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          headerShown: false,
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="home"
-              style={styles.iconos}
-              color={color}
-              size={30}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Habitaciones"
-        component={Habitaciones}
-        options={{
-          headerShown: false,
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="bed"
-              style={styles.iconos}
-              color={color}
-              size={30}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Reserva"
-        component={Home}
-        options={{
-          headerShown: false,
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="calendar-edit"
-              style={styles.iconos}
-              color={color}
-              size={30}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="User"
-        component={Home}
-        options={{
-          headerShown: false,
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="account-circle"
-              style={styles.iconos}
-              color={color}
-              size={30}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
-
-const styles = StyleSheet.create({
-  iconos: {
-    marginTop: 15,
-  },
-});
